0a4a0537ceb9ba2ab65857a3cac8336984298486
rad-vrc <dorado1031@gmail.com>
2025-09-29 20:36:51 +0900

    Fix cursor typing in delete-image function

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,12 +1,3 @@
-export const onRequest = async (context) => {
-    const url = new URL(context.request.url);
-    // 本番用 preview ドメインのみをリダイレクト対象に限定（他 preview は残す）
-    if (url.hostname === 'akyodex.pages.dev') {
-        const target = `https://akyodex.com${url.pathname}${url.search}`;
-        // 302にしてデバッグ/プレビュー時のキャッシュ固着を避ける
-        return Response.redirect(target, 302);
-    }
-    return context.next();
-};
+export const onRequest = (context) => context.next();
 
 
