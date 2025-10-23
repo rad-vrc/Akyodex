4badf13b39788b095b2dbf43a783f5cff425dbd7
rad-vrc <dorado1031@gmail.com>
2025-10-23 09:56:00 +0900

    Update eslint.config.mjs

--- akyodex-nextjs/eslint.config.mjs
+++ akyodex-nextjs/eslint.config.mjs
@@ -12,13 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    rules: {
+      // エラーを警告に変更
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@next/next/no-img-element": "warn",
+    },
   },
 ];
 
