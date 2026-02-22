13c333c3d345f5ef4105563d97143b91b7999553
rad-vrc <dorado1031@gmail.com>
2026-02-22 21:49:14 +0900

    chore(pr300): remove analysis artifacts and unrelated data diffs

--- check-insights.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('./lh-report-73.json', 'utf8'));
-const audits = data.audits;
-['render-blocking-insight', 'lcp-breakdown-insight', 'lcp-discovery-insight'].forEach(key => {
-    console.log(`\n=== ${key} ===`);
-    console.log(JSON.stringify(audits[key]?.details, null, 2));
-});
--- dump-audits.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('./lh-report-73.json', 'utf8'));
-const audits = data.audits;
-let out = '';
-for (const key in audits) {
-    const a = audits[key];
-    out += `${key} | ${a.title} | ${a.displayValue || ''} | ${a.score}\n`;
-}
-fs.writeFileSync('all-audits.txt', out);
